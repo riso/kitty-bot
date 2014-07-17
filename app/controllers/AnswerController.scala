528c15b79954ba5ecf6ce4175e386d3afc32000a
Valerio Ponte <valerio.ponte@gmail.com>
2014-07-17 22:38:38 +0200

    integrate catapi ws, change favicon and page title

--- app/controllers/AnswerController.scala
+++ app/controllers/AnswerController.scala
@@ -16,22 +16,28 @@ import models._
 
 object AnswerController extends Controller {
 
-  val answers = List("Meow!", "Purr", "Meow meow")
+  val answers = List("\"Meow!\"", "\"Purr\"")
 
   def generateAnswer(xs: List[String], r: Int): Future[Result]= xs match {
 	case x :: xs => r match {
 		case 0 => Future {Ok(Json.toJson(Answer(None, x)))}
 		case _ => generateAnswer(xs, r - 1)
 	}
-	case Nil => WS.url("http://catfacts-api.appspot.com/api/facts").get().map { response =>
-	    val json = Json.parse(response.body)
-		val fact = (json \ "facts")(0)
-	    Ok(Json.toJson(Answer(None, fact.toString()))) 
+	case Nil => r match {
+		case 1 => WS.url("http://catfacts-api.appspot.com/api/facts").get().map { response =>
+	      val json = Json.parse(response.body)
+		  val fact = (json \ "facts")(0)
+	      Ok(Json.toJson(Answer(None, fact toString))) 
+		}
+		case 0 => WS.url("http://thecatapi.com/api/images/get?format=xml").get().map { response =>
+		  val url = response.xml \\ "url"
+		  Ok(Json.toJson(Answer(None, "<img src='" + url.text + "'/>")))
+		}
 	  }
   }
 
   def index = Action.async { 
-	generateAnswer(answers, new Random().nextInt(answers.length + 1))
+	generateAnswer(answers, new Random().nextInt(answers.length + 2))
   }
 
 }
